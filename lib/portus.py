5967cef0315d27dc5b26295ec545a74655159a56
Richard Franks <git@rf152.co.uk>
2014-07-20 08:28:55 +0100

    Added GPRMC handling.
    The system now handles GPRMC sentences, which allow speed and course data to
    be collected.
    
    Also fixed that the time was being entered with just time, rather than with
    a date

--- lib/portus.py
+++ lib/portus.py
@@ -4,6 +4,7 @@ import serial;
 import re;
 import subprocess;
 import config;
+import time;
 
 
 def run():
@@ -34,7 +35,10 @@ def run():
 	
 	logger.debug("Waiting for GPRMC");
 	while line[1:6] != 'GPRMC':
-		line = port.readline().strip().decode("utf-8");
+		try:
+			line = port.readline().strip().decode("utf-8");
+		except UnicodeDecodeError:
+			pass
 		#print(line[1:6]);
 	
 	logger.debug("Got GPRMC");
@@ -46,6 +50,9 @@ def run():
 	d = m.group(2);
 	t = m.group(1);
 	dt = d[2:4] + d[0:2] + t[0:4] + d[4:] + '.' + t[4:];
+	
+	date = time.strftime("%Y")[0:2] + d[4:] + d[2:4] + d[0:2];
+	logger.debug(date);
 	
 	logger.debug("Setting date: " + dt);
 	nl = open("/dev/null", "w");
@@ -61,6 +68,8 @@ def run():
 #	ggapattern = '[A-Z]+,([0-9]{6}),([0-9\.]+),([A-Z]),([0-9\.]+),([A-Z]),([0-2]),([0-9]{2}),([0-9\.]+),([0-9\.]+),([0-9\.]+)';
 	ggapattern = '\$GPGGA,([0-9]{6}),([0-9\.]+),([NS]),([0-9\.]+),([EW]),([0-2]),([0-9]{2}),([0-9\.]+),([\-]?[0-9]+\.[0-9]),M,([\-]?[0-9]+\.[0-9])';
 	ggare = re.compile(ggapattern);
+	rmcpattern = '\$GPRMC,([0-9]{6}),[AV],[0-9\.]+,[NS],[0-9\.]+,[EW],([0-9\.]+),([0-3][0-9]{2}\.[0-9]),([0-9]{6}),([0-3][0-9]{2}\.[0-9]),([EW])';
+	rmcre = re.compile(rmcpattern);
 	
 	while True:
 		text = port.readline();
@@ -71,7 +80,7 @@ def run():
 			continue;
 		
 		sentence = text[1:6];
-		
+		logger.debug("Recieved {}".format(sentence));
 		if sentence == 'GPGGA':
 			# Positional data
 			try:
@@ -80,8 +89,20 @@ def run():
 				# Invalid data
 				continue;
 			parts = matches.groups();
+			parts = (date + parts[0],) + parts[1:];
 			db.cursor.execute('INSERT INTO points VALUES (?,?,?,?,?,?,?,?,?,?)', parts);
 			db.connection.commit();
-#		print(sentence);
-	#	print(text.strip());
+		elif sentence == 'GPRMC':
+			# Transit data
+			try:
+				matches = rmcre.search(text);
+			except AttributeError:
+				logging.debug("Invalid GPRMC pattern recieved");
+				continue;
+			parts = matches.groups();
+			d = parts[3]
+			date = time.strftime("%Y")[0:2] + d[4:] + d[2:4] + d[0:2];
+			parts = (date + parts[0],) + parts[1:3] + parts[4:]
+			db.cursor.execute('INSERT INTO transits VALUES (?,?,?,?,?)', parts);
+			db.connection.commit();
 
